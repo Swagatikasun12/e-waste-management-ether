ec299a2b55637bcb5be9508bca62508b5aca5460
Swagatika Sahoo <swagatikasun12@gmail.com>
2021-02-17 19:19:38 +0530

    Updated None Message.

--- frontend/src/pages/HomeNone.js
+++ frontend/src/pages/HomeNone.js
@@ -11,7 +11,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Oops! You don't belong here.</h1>
+                <h1>Oops! You don't belong here. Try Registering as a Stakeholder.</h1>
             </div>
         );
     }
