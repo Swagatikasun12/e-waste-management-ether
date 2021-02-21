b82b9a6ae3579d4063562afa2def01bf6ad8ffa7
SWAGATIKA SAHOO <66073961+Swagatikasun12@users.noreply.github.com>
2021-02-21 15:57:56 +0530

    Update TokenSc.sol

--- contracts/TokenSc.sol
+++ contracts/TokenSc.sol
@@ -3,29 +3,42 @@ contract Token  {
     uint public _d_id =0;
     uint public _u_id =0;
     uint public _token_id =1;
+    uint public _token_id1 =1;
     uint public _t_id=1;
     uint public tokenidd=0;
     
-    struct token1 
+      struct token1 
+    {
+       // string username;
+        uint[] userid;
+        address owner1;
+        uint[] _data_ids;
+        uint[] _token_ids;
+    }
+ 
+   mapping(uint => token1) public tokens;
+   
+    struct token2 
     {
         //string username;
         uint ownerid;
         uint rcvrid;
         //address owner1;
-        uint _data_ids;
+        uint _data_id;
         //uint _token_ids;
         uint timelimit;
         //uint now;
              
     }
-    mapping(uint => token1) public tokens1;
+    mapping(uint => token2) public tokens1;
+  // mapping(uint => token1) public tokens1;
  
-      function createToken(uint[] userid, uint[] _data_ids ,uint[] _token_ids) public returns (uint)
+      function createToken(uint[] memory _data_ids ,uint[] memory _token_ids) public returns (uint)
       {
-       for(uint u=0;u<userid.length;u++)
+       /*for(uint u=0;u<userid.length;u++)
        {
         tokens[u].userid.push(userid[u]);
-       }
+       }*/
         for(uint i=0;i<_data_ids.length;i++)
         {
             tokens[i]._data_ids.push(_data_ids[i]);
@@ -42,13 +55,17 @@ contract Token  {
       {
         tokens1[_token_id1].ownerid = ownerid1;
         tokens1[_token_id1].rcvrid = rcvrid1;
-        tokens1[_token_id1]._data_ids = _data_ids1;
+        tokens1[_token_id1]._data_id = _data_ids1;
         tokens1[_token_id1].timelimit = timelimit1;
           _token_id1++;
         return _token_id1;
     }
-    function getsplit(uint token2_id)public view returns(uint [], uint [])
+    function getsplit(uint token2_id)public view returns(uint [] memory, uint [] memory)
     {
         return (tokens[token2_id]._data_ids, tokens[token2_id]._token_ids);
+    }
+    function getsplittoken1(uint tokenn_id)public view returns(uint, uint, uint, uint )
+    {
+        return (tokens1[tokenn_id].ownerid, tokens1[tokenn_id].rcvrid, tokens1[tokenn_id]._data_id, tokens1[tokenn_id].timelimit);
     }
     }
